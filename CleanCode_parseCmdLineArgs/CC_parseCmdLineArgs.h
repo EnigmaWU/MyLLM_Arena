841e8a292750cbc019831bfa2081c60b243b9833
EnigmaWU <enigma0702@gmail.com>
2024-04-13 10:19:04 +0800

    initial interface code written by EnigmaWU and test code generated by GitHub Copilot

---
+++ CleanCode_parseCmdLineArgs/CC_parseCmdLineArgs.h
@@ -0,0 +1,21 @@
+
+typedef enum {
+  CC_SUCCESS = 0,
+  CC_FAIL    = 1,
+} CC_Result_T;
+
+typedef struct {
+  bool IsLoggingEnabled;  //-l
+  int RecvPort;           //-p <port>
+  char *pLogSavingDir;    //-d <dir>
+} CC_CmdLineArgs_T;
+
+/**
+ * @brief: Use CC_parseCmdLineArgs to parse command line arguments and save them in CC_CmdLingArgs_T.
+ *
+ * @param argc same as main
+ * @param argv same as main
+ * @param pCmdLineArgs pointer to CC_CmdLineArgs_T
+ * @return CC_SUCCESS if successful, CC_FAIL otherwise in CC_Result_T
+ */
+CC_Result_T CC_parseCmdLineArgs(int argc, char *argv[], CC_CmdLineArgs_T *pCmdLineArgs);
