085b955012fc8711e8e82f47be0a396a30e03ad2
EnigmaWU <enigma0702@gmail.com>
2024-04-14 20:55:57 +0800

    [GREEN] ChatCopilot 'fixup CC_parseCmdLinesArgs to pass TEST(CC_parseCmdLineArgs, InvalidArgs)'

--- CleanCode_parseCmdLineArgs/CC_parseCmdLineArgs.c
+++ CleanCode_parseCmdLineArgs/CC_parseCmdLineArgs.c
@@ -20,37 +20,34 @@ CC_Result_T CC_parseCmdLineArgs(int argc, char *argv[], CC_CmdLineArgs_T *pCmdLi
   pCmdLineArgs->RecvPort         = 0;
   pCmdLineArgs->pLogSavingDir    = NULL;
 
-  // If there are no arguments (except for the program name), return success
-  if (argc == 1) {
-    return CC_SUCCESS;
-  }
-
   // Parse the arguments
   for (int i = 1; i < argc; i++) {
-    if (argv[i][0] == '-') {
-      switch (argv[i][1]) {
-        case 'l':
-          pCmdLineArgs->IsLoggingEnabled = true;
-          break;
-        case 'p':
-          if (i + 1 < argc) {
-            pCmdLineArgs->RecvPort = atoi(argv[i + 1]);
-            i++;
-          } else {
-            return CC_FAIL;
-          }
-          break;
-        case 'd':
-          if (i + 1 < argc) {
-            pCmdLineArgs->pLogSavingDir = argv[i + 1];
-            i++;
-          } else {
-            return CC_FAIL;
-          }
-          break;
-        default:
+    if (argv[i][0] != '-') {
+      return CC_FAIL;
+    }
+
+    switch (argv[i][1]) {
+      case 'l':
+        pCmdLineArgs->IsLoggingEnabled = true;
+        break;
+      case 'p':
+        if (i + 1 < argc) {
+          pCmdLineArgs->RecvPort = atoi(argv[i + 1]);
+          i++;
+        } else {
           return CC_FAIL;
-      }
+        }
+        break;
+      case 'd':
+        if (i + 1 < argc) {
+          pCmdLineArgs->pLogSavingDir = argv[i + 1];
+          i++;
+        } else {
+          return CC_FAIL;
+        }
+        break;
+      default:
+        return CC_FAIL;
     }
   }
 
