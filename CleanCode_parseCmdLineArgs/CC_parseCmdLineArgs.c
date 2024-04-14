f736504fcbbf26cc658cff572eab4e92e5035097
EnigmaWU <enigma0702@gmail.com>
2024-04-14 19:43:58 +0800

    [GREEN-1] Implement CC_parseCmdLineArgs function to parse command line arguments

--- CleanCode_parseCmdLineArgs/CC_parseCmdLineArgs.c
+++ CleanCode_parseCmdLineArgs/CC_parseCmdLineArgs.c
@@ -1,3 +1,46 @@
 #include "CC_parseCmdLineArgs.h"
 
-CC_Result_T CC_parseCmdLineArgs(int argc, char *argv[], CC_CmdLineArgs_T *pCmdLineArgs) { return CC_FAIL; }
+#include <stddef.h>  // Include the header file that defines the NULL macro
+#include <stdlib.h>
+
+// ImplV1: Use Copilot to generate code to pass the test in CC_parseCmdLineArgsTest_byCopilot.cxx
+CC_Result_T CC_parseCmdLineArgs(int argc, char *argv[], CC_CmdLineArgs_T *pCmdLineArgs) {
+  if (argc < 7) {
+    return CC_FAIL;
+  }
+
+  pCmdLineArgs->IsLoggingEnabled = false;
+  pCmdLineArgs->RecvPort         = 0;
+  pCmdLineArgs->pLogSavingDir    = NULL;
+
+  for (int i = 1; i < argc; i++) {
+    if (argv[i][0] == '-') {
+      switch (argv[i][1]) {
+        case 'l':
+          pCmdLineArgs->IsLoggingEnabled = true;
+          break;
+        case 'p':
+          if (i + 1 < argc) {
+            pCmdLineArgs->RecvPort = atoi(argv[i + 1]);
+            i++;
+          } else {
+            return CC_FAIL;
+          }
+          break;
+        case 'd':
+          if (i + 1 < argc) {
+            pCmdLineArgs->pLogSavingDir = argv[i + 1];
+            i++;
+          } else {
+            return CC_FAIL;
+          }
+          break;
+        default:
+          return CC_FAIL;
+      }
+    } else {
+      return CC_FAIL;
+    }
+  }
+  return CC_SUCCESS;
+}
