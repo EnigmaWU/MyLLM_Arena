5bf9d6a234d2276cab8744558bc93c46acd0547b
EnigmaWU <enigma0702@gmail.com>
2024-04-14 19:55:17 +0800

    [GREEN-1] Fixup CC_parseCmdLineArgs function to pass TEST(CC_parseCmdLineArgs, NoArgs)

--- CleanCode_parseCmdLineArgs/CC_parseCmdLineArgs.c
+++ CleanCode_parseCmdLineArgs/CC_parseCmdLineArgs.c
@@ -5,14 +5,22 @@
 
 // ImplV1: Use Copilot to generate code to pass the test in CC_parseCmdLineArgsTest_byCopilot.cxx
 CC_Result_T CC_parseCmdLineArgs(int argc, char *argv[], CC_CmdLineArgs_T *pCmdLineArgs) {
-  if (argc < 7) {
-    return CC_FAIL;
-  }
-
+  // Initialize to default values
   pCmdLineArgs->IsLoggingEnabled = false;
   pCmdLineArgs->RecvPort         = 0;
   pCmdLineArgs->pLogSavingDir    = NULL;
 
+  // If there are no arguments (except for the program name), return success
+  if (argc == 1) {
+    return CC_SUCCESS;
+  }
+
+  // If there are not enough arguments, return fail
+  if (argc < 7) {
+    return CC_FAIL;
+  }
+
+  // Parse the arguments
   for (int i = 1; i < argc; i++) {
     if (argv[i][0] == '-') {
       switch (argv[i][1]) {
@@ -38,9 +46,8 @@ CC_Result_T CC_parseCmdLineArgs(int argc, char *argv[], CC_CmdLineArgs_T *pCmdLi
         default:
           return CC_FAIL;
       }
-    } else {
-      return CC_FAIL;
     }
   }
+
   return CC_SUCCESS;
 }
